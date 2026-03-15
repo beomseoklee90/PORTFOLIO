717f461fe380bbc54e989dd0bfc812560891f4d6
ボムソク\beoms <beomseoklee90@gmail.com>
2026-03-15 23:53:20 +0900

    finish

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  // basePath, assetPrefix 다 지우세요!
+  // 깃허브 페이지 배포를 위해 빌드 결과물을 정적으로 추출합니다.
+  output: 'export', 
+  // 이미지 최적화는 깃허브 서버가 못하니 꺼줍니다.
+  images: {
+    unoptimized: true,
+  },
+  // 주소 끝에 슬래시를 붙여서 경로 인식을 안정적으로 만듭니다.
   trailingSlash: true,
 };
+
 export default nextConfig;
