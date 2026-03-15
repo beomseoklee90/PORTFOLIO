da71e40d42ad6bf7f06a733e1930b1b09cde6d29
ボムソク\beoms <beomseoklee90@gmail.com>
2026-03-15 23:39:44 +0900

    Updates

--- _next/static/sQEZ4UgR9IeojIC8T8fPk/_buildManifest.js
+++ _next/static/sQEZ4UgR9IeojIC8T8fPk/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/PORTFOLIO//_next/:path+",
-        "destination": "/PORTFOLIO/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
