5766eeeb4453e98357717df99576fd1d5fadb760
ボムソク\beoms <beomseoklee90@gmail.com>
2026-03-16 17:19:03 +0900

    typography changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+// layout.tsx
+import React from "react";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,7 +9,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body className="m-0 p-0 overflow-hidden bg-[#0a0a0a]">{children}</body>
+      {/* className에 폰트 변수 넣지 말고 그냥 비워두세요. globals.css의 * { ... } 가 잡을 겁니다. */}
+      <body>{children}</body>
     </html>
   );
 }
